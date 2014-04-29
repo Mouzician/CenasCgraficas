f4bd5187a5a7879758addb2409354cb201b76cce
Psouka <p4sousa@gmail.com>
2014-04-29 16:18:17 +0100

    asd

---
+++ CGRA/src/LightingScene.h
@@ -0,0 +1,39 @@
+#ifndef LightingScene_H
+#define LightingScene_H
+
+#include "CGFscene.h"
+#include "CGFappearance.h"
+#include "myTable.h"
+#include "Plane.h"
+#include "myCylinder.h"
+
+class LightingScene : public CGFscene
+{
+public:
+	void init();
+	void display();
+
+	CGFlight* light0;
+	CGFlight* light1;
+	CGFlight* light2;
+	CGFlight* light3;
+	CGFlight* light4;
+
+	myTable* table;
+	Plane* wall;
+	Plane* boardA;
+	Plane* boardB;
+	CGFappearance* materialA;
+	CGFappearance* materialB;
+	myCylinder* cylinder;
+	myCylinder* cylinder2;
+	CGFappearance* windowAppearance;
+	CGFappearance* floorAppearance;
+	CGFappearance* slidesAppearance;
+	CGFappearance* boardAppearance;
+	CGFappearance* clockAppearance;
+
+	~LightingScene();
+};
+
+#endif
